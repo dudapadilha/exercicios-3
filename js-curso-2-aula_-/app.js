7f7e57e7005d77ecd1db8e55a97eb19c226b5d05
unknown <Maria Eduarda Padilha Baltazar>
2024-06-20 14:18:13 -0300

    exercicios 3

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,31 @@
+// Ex. 1
+
+let listaGenerica = [];
+
+// Ex. 2
+
+let listaLinguagens = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+// Ex. 3
+
+listaLinguagens.push('Java', 'Ruby', 'GoLang');
+console.log(listaLinguagens);
+
+// Ex. 4
+
+let listaNomes = ['duda', 'madu', 'maria'];
+console.log(listaNomes[0]); 
+
+// Ex. 5
+
+let listaNomesDois = ['duda', 'madu', 'maria'];
+console.log(listaNomesDois[1]); 
+
+// Ex. 6
+
+let listaNomesTres = ['duda', 'madu', 'maria'];
+console.log(listaNomesTres[2]); 
+
+
+
+  
